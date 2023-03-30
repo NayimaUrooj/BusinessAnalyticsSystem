ce4970face1a3bd6a14125e02d93c18726ece0fa
Nayima Urooj <nayimaurooj@gmail.com>
2023-03-30 10:54:17 +0200

    RTK query

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -7,10 +7,15 @@ import { useSelector } from 'react-redux';
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
 import useTheme from '@mui/material';
+import { useGetUserQuery } from 'state/api';
+
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+const {data} = useGetUserQuery(userId);
+console.log(data, 'data');
 
   return (
     <Box display = {isNonMobile ? "flex" : "block"} width = "100%" height = "100%" >
