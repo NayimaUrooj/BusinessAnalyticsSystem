22dc65ea370935eac4a29b2ece2673120b583bff
Nayima Urooj <nayimaurooj@gmail.com>
2023-05-02 21:01:39 +0200

    fix - a bug

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,9 @@ import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
 
 
 function App() {
@@ -39,8 +42,11 @@ function App() {
             <Route path= "/products" element={<Products/>} />
             <Route path= "/customers" element={<Customers/>} />
             <Route path= "/transactions" element={<Transactions/>} />
-            <Route path= "/Geography" element={<Geography/>} />
-            <Route path= "/Overview" element={<Overview/>} />
+            <Route path= "/geography" element={<Geography/>} />
+            <Route path= "/overview" element={<Overview/>} />
+            <Route path= "/daily" element={<Daily/>} />
+            <Route path= "/monthly" element={<Monthly/>} />
+            <Route path= "/breakdown" element={<Breakdown/>} />
           </Route>
         </Routes>
       </ThemeProvider>
