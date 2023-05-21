672a09eb6da1f2eed751661fa50fb161f9335bda
Nayima Urooj <nayimaurooj@gmail.com>
2023-05-21 20:39:35 +0200

    fix - eslint

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
-module.exports = {
-  presets: ['react-app'],
+require('dotenv').config();
+
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+        modules: "commonjs",
+      },
+    ],
+    "@babel/preset-react",
+  ];
+
+  return {
+    presets,
+  };
 };
